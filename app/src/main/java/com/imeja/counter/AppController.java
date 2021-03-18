743a06bacd8088042d899ffcd30dce6bccdb879f
keeprawteach <keeprawteachjapheth@gmail.com>
2021-03-18 15:21:01 +0300

    first commit

---
+++ app/src/main/java/com/imeja/counter/AppController.java
@@ -0,0 +1,6 @@
+package com.imeja.counter;
+
+import android.app.Application;
+
+public class AppController extends Application {
+}
---
+++ app/src/main/java/com/imeja/counter/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.imeja.counter.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.imeja.counter.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
